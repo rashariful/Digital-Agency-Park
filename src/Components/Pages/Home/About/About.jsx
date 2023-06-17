f739887b26dae6173b9969bfc6bf9d0f52b4f22a
unknown <ra.mdsharifulislam@gmail.com>
2023-06-17 20:23:23 +0600

    change name

--- src/Components/Pages/Home/About/About.jsx
+++ src/Components/Pages/Home/About/About.jsx
@@ -41,19 +41,19 @@ const About = () => {
 
             <div class="md:pt-8">
               <h1 class="mb-4 text-center text-2xl font-bold text-gray-800 sm:text-3xl md: md:text-left">
-                DhakaCars: Revolutionizing Car Rentals in Dhaka, Bangladesh
+                Digital Agency Park: Revolutionizing 
               </h1>
               <p class="text-center text-lg font-bold md:text-left">
                 Introduction:
               </p>
 
               <p class=" text-gray-500 sm:text-lg md:mb-8">
-                DhakaCars, an innovative car rental company based in Dhaka,
+                Digital Agency Park, an innovative 
                 Bangladesh, is transforming the way people access convenient and
                 reliable transportation services. With a mission to provide
-                hassle-free and affordable car rentals, DhakaCars aims to
+                hassle-free and affordable car rentals, Digital Agency Park aims to
                 empower individuals and businesses to navigate the bustling
-                streets of Dhaka with ease. This article explores DhakaCars'
+                streets of Dhaka with ease. This article explores Digital Agency Park'
                 vision, mission, and their commitment to revolutionizing the car
                 rental industry in Bangladesh.
               </p>
@@ -62,9 +62,9 @@ const About = () => {
               </p>
 
               <p class=" text-gray-500 sm:text-lg md:mb-8">
-                DhakaCars' mission is to redefine car rentals in Dhaka, offering
+                Digital Agency Park' mission is to redefine car rentals in Dhaka, offering
                 customers a seamless and convenient experience. By leveraging
-                technology and providing exceptional customer service, DhakaCars
+                technology and providing exceptional customer service, Digital Agency Park
                 aims to be the go-to platform for reliable, affordable, and
                 efficient transportation solutions in the city. The company
                 strives to exceed customer expectations by delivering
@@ -74,23 +74,23 @@ const About = () => {
               <p class="text-center text-lg font-bold md:text-left">Vision:</p>
 
               <p class=" text-gray-500 sm:text-lg md:mb-8">
-                DhakaCars envisions a future where transportation in Dhaka is no
+                Digital Agency Park envisions a future where transportation in Dhaka is no
                 longer a challenge, but a hassle-free experience. The company
                 aims to create a culture of accessibility and convenience by
                 offering a wide range of vehicles for various needs and budgets.
                 Through continuous innovation and technological advancements,
-                DhakaCars seeks to become the preferred choice for locals and
+                Digital Agency Park seeks to become the preferred choice for locals and
                 tourists alike, ensuring seamless mobility solutions throughout
                 the city.
               </p>
               <p class="text-center text-lg font-bold md:text-left">Our Aim:</p>
               <p class="text-gray-500 sm:text-lg">
-                DhakaCars is dedicated to fulfilling several key aims:
+                Digital Agency Park is dedicated to fulfilling several key aims:
               </p>
               <p>
                 <span className="text-md font-semibold text-slate-700">1.</span>
                 <span className="ml-2 font-bold text-black">Convennience:</span>
-                DhakaCars strives to make car rentals as effortless as possible,
+                Digital Agency Park strives to make car rentals as effortless as possible,
                 ensuring a streamlined booking process and providing customers
                 with easy access to a diverse fleet of vehicles. Through an
                 intuitive online platform, users can conveniently reserve a car
@@ -102,7 +102,7 @@ const About = () => {
                   Affordability:
                 </span>
                 <span className="text-gray-500 ml-2">
-                DhakaCars believes that reliable transportation should be
+                Digital Agency Park believes that reliable transportation should be
                 accessible to everyone. By offering competitive pricing and
                 flexible rental options, the company aims to provide
                 cost-effective solutions that cater to various budgets without
@@ -114,11 +114,11 @@ const About = () => {
                   Exceptional Service:
                 </span>
                 <span className="text-gray-500 ml-2">
-                DhakaCars places great emphasis on customer satisfaction. Their
+                Digital Agency Park places great emphasis on customer satisfaction. Their
                 dedicated team is committed to delivering top-notch service,
                 ensuring that customers receive a warm welcome, personalized
                 assistance, and timely support throughout their rental
-                experience. By going the extra mile, DhakaCars aims to build
+                experience. By going the extra mile, Digital Agency Park aims to build
                 long-term relationships with their customers.</span>
               </p>
               <p>
@@ -127,7 +127,7 @@ const About = () => {
                   Quality and Safety:
                 </span>
                 <span className="text-gray-500 ml-2">
-                DhakaCars prioritizes the safety and comfort of their customers.
+                Digital Agency Park prioritizes the safety and comfort of their customers.
                 Every vehicle in their fleet undergoes regular maintenance and
                 inspection to ensure optimal performance and reliability.
                 Additionally, the company follows stringent safety protocols,
@@ -139,18 +139,18 @@ const About = () => {
                   Community Impact:
                 </span>
                 <span className="text-gray-500 ml-2">
-                DhakaCars recognizes its role in contributing to the local
+                Digital Agency Park recognizes its role in contributing to the local
                 community and economy. By creating job opportunities and
                 collaborating with local partners, the company aims to foster
                 sustainable growth and make a positive social impact in Dhaka
                 and beyond. </span>
               </p>
               <p className="text-gray-500">
-                In conclusion, DhakaCars is revolutionizing the car rental
+                In conclusion, Digital Agency Park is revolutionizing the car rental
                 industry in Dhaka, Bangladesh, by prioritizing convenience,
                 affordability, exceptional service, quality, and safety. With
                 their innovative approach, commitment to customer satisfaction,
-                and dedication to the community, DhakaCars is well on its way to
+                and dedication to the community, Digital Agency Park is well on its way to
                 becoming the premier car rental platform in Dhaka, empowering
                 individuals and businesses to navigate the city effortlessly.
               </p>
