ad8e4ee99513fc166c8708322c5fca8efe329c64
unknown <ra.mdsharifulislam@gmail.com>
2023-08-09 17:35:28 +0600

    add custome facebook chat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
 
 function App() {
   return (
     <div className="App font-myFont">
+      <MessengerCustomerChat
+       pageId="106055157950858"
+       appId="199380903116422"
+       
+      >
+
     <RouterProvider router={router}></RouterProvider>
+      </MessengerCustomerChat>
+   
+ 
     </div>
   );
 }
